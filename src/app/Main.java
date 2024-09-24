5c8303f579bfed1a1b33225aa6ec1a66c7fde0a8
Pavel Petronjuk <hardboatguy@gmail.com>
2024-09-24 16:03:06 +0300

    add test remark on develop branch;

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,14 +5,21 @@ public class Main {
     private static final double CONV_K = 2.20462;
 
     public static void main(String[] args) {
-        System.out.println("App for measures converting.");
+        System.out.println("Version 1.0.");
         double kgs = 5;
+        double pnds = 12;
         double pounds = convKgsToPounds(kgs);
-        System.out.println("result is " + pounds + " pounds.");
+        double kilos = convPoundsToKgs(pnds);
+        System.out.println("result is " + pounds + " pounds and " + kilos + " kgs.");
+        //1
     }
 
     private static double convKgsToPounds(double kgs)
     {
         return kgs * CONV_K;
+    }
+    private static double convPoundsToKgs(double pnds)
+    {
+        return pnds / CONV_K;
     }
 }
